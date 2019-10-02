23dc52cdeb8780d711bebecb752bc86e1895ee5b
Jochem Brouwer <jochembrouwer96@gmail.com>
2019-10-02 17:55:50 +0200

    tram update

--- src/org/uu/mads/simulation/Tram.java
+++ src/org/uu/mads/simulation/Tram.java
@@ -7,4 +7,11 @@ public class Tram {
 	private int numOfPassengers;
 
 	// Getters and setters are here
+
+  // loads/unloads passengers on a platform and returns the dwell time of the tram 
+  public double loadPassengers(IntPlatform platform) {
+    this.numOfPassengers = this.numOfPassengers 
+
+    return 12.5; // todo change this to actual dwell time (random)
+  }
 }
