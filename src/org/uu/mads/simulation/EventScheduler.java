69913ee87639b6c22421f0c0b24be03a01d091d7
jokke <jokke@jokke-ThinkPad-T440>
2019-10-03 11:16:47 +0200

    Implement scheduling methods

--- src/org/uu/mads/simulation/EventScheduler.java
+++ src/org/uu/mads/simulation/EventScheduler.java
@@ -2,33 +2,52 @@ package org.uu.mads.simulation;
 
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class EventScheduler {
-	private final SortedMap<LocalTime, List<Event>> events = new TreeMap<>(); // needs to be sorted by time and priority
+	private final SortedMap<LocalTime, List<Event>> scheduledEventsByTime = new TreeMap<>(); // needs to be sorted by
+																								// time and
+	// priority
 	private final LocalTime currentTime;
 
 	public EventScheduler(final LocalTime currentTime) {
 		this.currentTime = currentTime;
 	}
 
-
 	public LocalTime getCurrentTime() {
 		return this.currentTime;
 	}
 
-	public void scheduleEvent(final Event event, final LocalTime time) {
-		// TODO: implement
+	public void scheduleEvent(final Event event, final LocalTime eventTime) {
+		final List<Event> scheduledEvents;
 
+		if (this.scheduledEventsByTime.containsKey(eventTime)) {
+			scheduledEvents = this.scheduledEventsByTime.get(eventTime);
+			for (int i = 0; i < scheduledEvents.size(); i++) {
+				final Event scheduledEvent = scheduledEvents.get(i);
+				if (scheduledEvent.getPriority() < event.getPriority()) {
+					scheduledEvents.add(i, event);
+				}
+			}
+			if (!scheduledEvents.contains(event)) {
+				scheduledEvents.add(event);
+			}
+		} else {
+			scheduledEvents = new LinkedList<>();
+			scheduledEvents.add(event);
+		}
+		this.scheduledEventsByTime.put(eventTime, scheduledEvents);
 	}
 
 	public void scheduleEventAhead(final Event event, final Duration duration) {
-		// TODO: implement
+		final LocalTime eventTime = this.currentTime.plus(duration);
+		scheduleEvent(event, eventTime);
 	}
-	
-	public static void main(String[] args) {
+
+	public static void main(final String[] args) {
 		System.out.println("hi");
 	}
 }
