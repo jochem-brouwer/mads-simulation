29f27582550ca4661f27c5e288d510361ecf0096
jokke <jokke@jokke-ThinkPad-T440>
2019-10-06 17:26:57 +0200

    Add first tram leave time

--- src/org/uu/mads/simulation/Simulation.java
+++ src/org/uu/mads/simulation/Simulation.java
@@ -1,8 +1,10 @@
 package org.uu.mads.simulation;
 
 import java.time.Duration;
+import java.time.LocalTime;
 
 public class Simulation {
+	public static final LocalTime FIRST_TRAM_LEAVE_TIME = LocalTime.of(7, 0); // TODO: Adapt
 	public static final Duration TRAM_LEAVE_FREQUENCY = Duration.ofMinutes(5); // TODO: Adapt
 
 	public static void main(final String[] args) {
