cf11ed22e626ee0586355bfa0906a439e536f632
jokke <jokke@jokke-ThinkPad-T440>
2019-10-24 18:41:43 +0200

    Make that constant actually use the correct number

--- src/org/uu/mads/simulation/Simulation.java
+++ src/org/uu/mads/simulation/Simulation.java
@@ -36,7 +36,7 @@ public class Simulation {
 	public static final LocalTime FIRST_SCHEDULED_LEAVE_TIME_PR = LocalTime.of(6, 0);
 	public static final LocalTime FIRST_PASSENGER_CALC = LocalTime.of(6, 0);
 	public static final int NUMBER_OF_TRAMS = 16; // number of trams we want to deploy
-	public static final int INITIAL_NUMBER_OF_TRAMS_CS = (NUMBER_OF_TRAMS % 2) == 0 ? ((NUMBER_OF_TRAMS / 2))
+	public static final int INITIAL_NUMBER_OF_TRAMS_CS = (NUMBER_OF_TRAMS % 2) == 0 ? (((NUMBER_OF_TRAMS / 2) - 1))
 			: ((NUMBER_OF_TRAMS / 2));
 	public static final Duration TRAM_LEAVE_FREQUENCY = Duration.ofSeconds(3600 / NUMBER_OF_TRAMS);
 	public static final Duration AVG_ONE_WAY_DRIVING_TIME = Duration.ofMinutes(17);
